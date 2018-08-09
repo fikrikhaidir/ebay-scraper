3d874ef6e21a275e195b54b86ca1af01d75908fb
vandalicious <fikrikhaidir@gmail.com>
2018-08-09 14:30:59 +0700

    index page

--- processor/bs.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-from bs4 import BeautifulSoup
-import requests
-
-# res = requests.get('https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=lenovo&_sacat=0')
-# soup = BeautifulSoup(res.content)
